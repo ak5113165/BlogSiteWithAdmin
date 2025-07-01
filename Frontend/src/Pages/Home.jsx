f41543d643e72d27969ebe5e1b183bd1442ff5a7
Ankit Kumar <GFG19632@gfg.geeksforgeeks.org>
2025-07-02 01:30:05 +0530

    project completed

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Header from '../Components/Header'
+import BlogList from '../Components/BlogList' 
+import NewsLetter from '../Components/NewsLetter' 
+import Footer from '../Components/Footer' 
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <BlogList/>
+    <NewsLetter/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
